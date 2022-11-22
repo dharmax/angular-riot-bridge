de7d6eb3632a20c223ac28f728fc145ca0f2c421
Avi Tshuva <avi.tshuva@edr.dom>
2022-11-22 16:51:04 +0200

    baseline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RiotContainerComponent } from './riot-container/riot-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RiotContainerComponent
   ],
   imports: [
     BrowserModule
